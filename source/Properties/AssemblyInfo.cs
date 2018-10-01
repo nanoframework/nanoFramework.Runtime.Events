fa32b4e06e8effa52a96bdc0181913240735341c
nfbot <nfbot@users.noreply.github.com>
2018-10-01 01:21:40 +0000

    Update assembly info file for v1.0.0-preview198

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.196")]
-[assembly: AssemblyVersion("1.0.0.196")]
-[assembly: AssemblyFileVersion("1.0.0.196")]
-[assembly: AssemblyInformationalVersion("1.0.0-preview196")]
+// [assembly: AssemblyVersion("1.0.0.198")]
+[assembly: AssemblyVersion("1.0.0.198")]
+[assembly: AssemblyFileVersion("1.0.0.198")]
+[assembly: AssemblyInformationalVersion("1.0.0-preview198")]
